32a9792d8ba3a65b634948b4b25a1bc9cf1d5d0c
Florian Zand <florianzand@me.com>
2023-12-01 12:17:28 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
         .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Anima",
             targets: ["Anima"]),
@@ -20,8 +19,6 @@ let package = Package(
         .package(url: "https://github.com/b3ll/Decomposed.git", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Anima",
             dependencies: ["Decomposed"]),
