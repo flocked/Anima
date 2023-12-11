563d54047e37e4c35f01b96624f9f247fc26f35d
Florian Zand <florianzand@me.com>
2023-12-11 02:10:08 +0100

    Update Spring.swift

--- Sources/Anima/Animations/Spring/Spring.swift
+++ Sources/Anima/Animations/Spring/Spring.swift
@@ -117,9 +117,7 @@ public struct Spring: Sendable, Hashable {
 
         let unbandedDampingCoefficient = Self.damping(dampingRatio: dampingRatio, response: response, mass: mass)
         
-    //    self.damping = Rubberband.value(for: unbandedDampingCoefficient, range: 0 ... 60, interval: 15)
-        
-        self.damping = unbandedDampingCoefficient
+        self.damping = Rubberband.value(for: unbandedDampingCoefficient, range: 0 ... 60, interval: 15)
         
         self.settlingDuration = Self.settlingTime(dampingRatio: dampingRatio, damping: damping, stiffness: stiffness, mass: mass)
     }
